c42e4b6c4e49862eb3bc4061b814161e55ab08c4
Lorenzo Vivenzio <lorenzovivenzio988@hotmail.com>
2025-11-25 16:48:36 +0100

    add header

--- components/AppHeader.jsx
+++ components/AppHeader.jsx
@@ -1,6 +1,24 @@
 export default function AppHeader(){
     return (
-    <header>
+
+    <header className=" cont align-center d-flex justify-content-beetwen">
+        <div className="logo-img">
+            <img src="/img/dc-logo.png" alt="" />
+        </div>
+        <div className="header-menu">
+            <ul className="d-flex">
+                <li><a href="">Characters</a></li>
+                <li><a href="">comics</a></li>
+                <li><a href="">Moves</a></li>
+                <li><a href="">Tv</a></li>
+                <li><a href="">Games</a></li>
+                <li><a href="">Collectibles</a></li>
+                <li><a href="">Videos</a></li>
+                <li><a href="">Fans</a></li>
+                <li><a href="">News</a></li>
+                <li><a href="">Shop</a></li>
+            </ul>
+        </div>
         
     </header>)
 }
