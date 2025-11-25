2d4f66847db131b891130ac4048efe7ed8b263fe
Lorenzo Vivenzio <lorenzovivenzio988@hotmail.com>
2025-11-25 18:34:46 +0100

    add correzione cartella

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import AppHeader from '../components/AppHeader'
-import AppMain from '../components/AppMain'
-import AppListMain from '../components/AppListMain'
-import AppFooterList from '../components/AppFooterList'
+import AppHeader from './components/AppHeader'
+import AppMain from './components/AppMain'
+import AppListMain from './components/AppListMain'
+import AppFooterList from './components/AppFooterList'
 
 function App() {
   return (
